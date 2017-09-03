48468fbee20c246792f84ddbc52d91c510ce8c5f
ivcandela <ivankcandela@gmail.com>
2017-09-03 20:12:50 +0200

    extracted transcoding into its own service

---
+++ worker/services/transcoding.js
@@ -0,0 +1,49 @@
+const FFmpeg = require('ffmpeg');
+const _ = require('lodash');
+const fileExists = require('file-exists');
+const fs = require('fs');
+
+const logging = require('../lib/logging');
+
+const LOGO_IMAGE_FILEPATH = './logo/logo.png';
+const LOGO_POSITION_FILEPATH = './logo/logo.json';
+
+const _watermark = function (videoIn, videoInFilename, logo, logoPos) {
+    return new Promise(function (resolve, reject) {
+        videoIn.fnAddWatermark(logo, './output/' + videoInFilename, logoPos, function (error, file) {
+            if (!error) {
+                resolve(file);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
+
+const transcode = (videoFilename) => {
+    try {
+        const transcodingProcess = new FFmpeg(videoFilename);
+        transcodingProcess.then(function (video) {
+            if (!fileExists.sync(LOGO_IMAGE_FILEPATH)) {
+                console.error('No logo.json specified for video ' + videoFilename);
+                return;
+            }
+            if (!fileExists.sync(LOGO_POSITION_FILEPATH)) {
+                console.error('No logo.json specified for video' + videoFilename);
+                return;
+            }
+            const logoPosObj = JSON.parse(fs.readFileSync(LOGO_POSITION_FILEPATH, 'utf8'));
+            _watermark(video, videoFilename, LOGO_IMAGE_FILEPATH, logoPosObj).then( videoOutFilename => {
+                return videoOutFilename;
+            });
+        }, function (err) {
+            console.error('Error: ' + err);
+        });
+    } catch (err) {
+        console.error('Error: ' + err);
+    }
+};
+
+module.exports = {
+    transcode,
+};
