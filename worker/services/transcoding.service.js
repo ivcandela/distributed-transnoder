8c76eb0442d052338cb5e65929af8fd306061ef1
ivcandela <ivankcandela@gmail.com>
2017-09-07 01:19:24 +0200

    updated transcoding service to make watermarking work

--- worker/services/transcoding.service.js
+++ worker/services/transcoding.service.js
@@ -1,16 +1,17 @@
 const FFmpeg = require('ffmpeg');
 const _ = require('lodash');
 const fileExists = require('file-exists');
+const path = require('path');
 const fs = require('fs');
 
 const logging = require('../lib/logging');
 
-const LOGO_IMAGE_FILEPATH = './logo/logo.png';
-const LOGO_POSITION_FILEPATH = './logo/logo.json';
+const LOGO_IMAGE_FILEPATH = path.join(__dirname, '..', 'logo', 'logo.png');
+const LOGO_POSITION_FILEPATH = path.join(__dirname, '..', 'logo', 'logo.json');
 
 const _watermark = function (videoIn, videoInFilename, logo, logoPos) {
     return new Promise(function (resolve, reject) {
-        videoIn.fnAddWatermark(logo, './output/' + videoInFilename, logoPos, function (error, file) {
+        videoIn.fnAddWatermark(logo, path.join(__dirname, '..', 'tmp', 'out_' + path.basename(videoInFilename)), logoPos, function (error, file) {
             if (!error) {
                 resolve(file);
             } else {
@@ -20,27 +21,27 @@ const _watermark = function (videoIn, videoInFilename, logo, logoPos) {
     });
 };
 
-const transcode = (videoFilename) => {
+const transcode = async (videoFilename) => {
     try {
         const transcodingProcess = new FFmpeg(videoFilename);
-        transcodingProcess.then(function (video) {
-            if (!fileExists.sync(LOGO_IMAGE_FILEPATH)) {
-                console.error('No logo.json specified for video ' + videoFilename);
-                return;
-            }
-            if (!fileExists.sync(LOGO_POSITION_FILEPATH)) {
-                console.error('No logo.json specified for video' + videoFilename);
-                return;
-            }
-            const logoPosObj = JSON.parse(fs.readFileSync(LOGO_POSITION_FILEPATH, 'utf8'));
-            _watermark(video, videoFilename, LOGO_IMAGE_FILEPATH, logoPosObj).then( videoOutFilename => {
-                return videoOutFilename;
-            });
-        }, function (err) {
-            console.error('Error: ' + err);
-        });
+        const video = await transcodingProcess;
+        logging.info('logo.png fileName', LOGO_IMAGE_FILEPATH);
+        logging.info('logo.json fileName', LOGO_POSITION_FILEPATH);
+        if (!fileExists.sync(LOGO_IMAGE_FILEPATH)) {
+            logging.error('No logo.png specified for video ' + videoFilename);
+            return;
+        }
+        if (!fileExists.sync(LOGO_POSITION_FILEPATH)) {
+            logging.error('No logo.json specified for video' + videoFilename);
+            return;
+        }
+        const logoPosObj = JSON.parse(fs.readFileSync(LOGO_POSITION_FILEPATH, 'utf8'));
+        logging.info('starting watermarking');
+        const videoOutFilename = await _watermark(video, videoFilename, LOGO_IMAGE_FILEPATH, logoPosObj);
+        logging.info('videoOut', videoOutFilename);
+        return videoOutFilename;
     } catch (err) {
-        console.error('Error: ' + err);
+        logging.error('Error: ' + err);
     }
 };
 
